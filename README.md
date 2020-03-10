51eb89e51faf0e6393cda3c6ef7e988809884302
Sam Deane <sam@elegantchaos.com>
2020-03-10 10:45:15 +0000

    use mirroring instead

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 The hackintosh that I run is great, but it gets a bit confused about displays when it first starts up.
 
-All I need to do to fix it is to change the display mode of one of the displays.
-
-All this app does is to change the mode of the main display, then change it back again.
+All I need to do to fix it is to turn mirroring on and off again, so that's what this app does.
--- ToggleDisplayModes/AppDelegate.swift
+++ ToggleDisplayModes/AppDelegate.swift
@@ -13,8 +13,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-        toggleModes()
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now().advanced(by: .seconds(10))) {
+        toggleMirroring {
             NSApp.terminate(self)
         }
     }
@@ -23,7 +22,48 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
-    func toggleModes() {
+    func getDisplays() -> [CGDirectDisplayID] {
+        var displayCount: UInt32 = 0;
+        if CGGetActiveDisplayList(0, nil, &displayCount) == CGError.success {
+            let allocated = Int(displayCount)
+            let displays = UnsafeMutablePointer<CGDirectDisplayID>.allocate(capacity: allocated)
+            defer { displays.deallocate() }
+
+            if CGGetActiveDisplayList(displayCount, displays, &displayCount) == CGError.success {
+                var result: [CGDirectDisplayID] = []
+                for i in 0 ..< displayCount {
+                    result.append(displays[Int(i)])
+                }
+                return result
+            }
+        }
+        
+        return []
+    }
+    
+    func toggleMirroring(completion: @escaping () -> (Void)) {
+        let displays = getDisplays()
+        let main = CGMainDisplayID()
+        
+        for display in displays {
+            if display != main {
+                var token: CGDisplayConfigRef?
+                CGBeginDisplayConfiguration(&token)
+                CGConfigureDisplayMirrorOfDisplay(token, display, main)
+                CGCompleteDisplayConfiguration(token, .forSession)
+                
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now().advanced(by: .seconds(10))) {
+                    CGBeginDisplayConfiguration(&token)
+                    CGConfigureDisplayMirrorOfDisplay(token, display, kCGNullDirectDisplay)
+                    CGCompleteDisplayConfiguration(token, .forSession)
+                    completion()
+                }
+                break
+            }
+        }
+    }
+    
+    func toggleModes(completion: @escaping () -> (Void)) {
         let display = CGMainDisplayID()
         var token: CGDisplayConfigRef?
         let current = CGDisplayCopyDisplayMode(display)
@@ -33,14 +73,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                     CGBeginDisplayConfiguration(&token)
                     CGDisplaySetDisplayMode(display, mode, nil)
                     CGCompleteDisplayConfiguration(token, .forSession)
+                    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now().advanced(by: .seconds(10))) {
+                        CGBeginDisplayConfiguration(&token)
+                        CGDisplaySetDisplayMode(display, current, nil)
+                        CGCompleteDisplayConfiguration(token, .forSession)
+                        completion()
+                    }
                     break
                 }
             }
         }
         
-        CGBeginDisplayConfiguration(&token)
-        CGDisplaySetDisplayMode(display, current, nil)
-        CGCompleteDisplayConfiguration(token, .forSession)
     }
 
 }
