995b3435f4a06aab4638c7032ceb8a2d8370eed7
Sam Deane <sam@elegantchaos.com>
2020-03-10 12:23:41 +0000

    moved display code into package

--- ToggleDisplayModes/AppDelegate.swift
+++ ToggleDisplayModes/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import Displays
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -22,40 +23,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
-    func getDisplays() -> [CGDirectDisplayID] {
-        var displayCount: UInt32 = 0;
-        if CGGetActiveDisplayList(0, nil, &displayCount) == CGError.success {
-            let allocated = Int(displayCount)
-            let displays = UnsafeMutablePointer<CGDirectDisplayID>.allocate(capacity: allocated)
-            defer { displays.deallocate() }
-
-            if CGGetActiveDisplayList(displayCount, displays, &displayCount) == CGError.success {
-                var result: [CGDirectDisplayID] = []
-                for i in 0 ..< displayCount {
-                    result.append(displays[Int(i)])
-                }
-                return result
-            }
-        }
-        
-        return []
-    }
-    
     func toggleMirroring(completion: @escaping () -> (Void)) {
-        let displays = getDisplays()
-        let main = CGMainDisplayID()
+        let displays = Display.active
+        let main = Display.main
         
         for display in displays {
             if display != main {
-                var token: CGDisplayConfigRef?
-                CGBeginDisplayConfiguration(&token)
-                CGConfigureDisplayMirrorOfDisplay(token, display, main)
-                CGCompleteDisplayConfiguration(token, .forSession)
+                display.mirror(to: main)
                 
                 DispatchQueue.main.asyncAfter(deadline: DispatchTime.now().advanced(by: .seconds(10))) {
-                    CGBeginDisplayConfiguration(&token)
-                    CGConfigureDisplayMirrorOfDisplay(token, display, kCGNullDirectDisplay)
-                    CGCompleteDisplayConfiguration(token, .forSession)
+                    display.unmirror()
                     completion()
                 }
                 break
@@ -64,27 +41,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     func toggleModes(completion: @escaping () -> (Void)) {
-        let display = CGMainDisplayID()
-        var token: CGDisplayConfigRef?
-        let current = CGDisplayCopyDisplayMode(display)
-        if let cgModes = CGDisplayCopyAllDisplayModes(display, nil), let modes = cgModes as? Array<CGDisplayMode> {
-            for mode in modes {
-                if mode != current {
-                    CGBeginDisplayConfiguration(&token)
-                    CGDisplaySetDisplayMode(display, mode, nil)
-                    CGCompleteDisplayConfiguration(token, .forSession)
-                    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now().advanced(by: .seconds(10))) {
-                        CGBeginDisplayConfiguration(&token)
-                        CGDisplaySetDisplayMode(display, current, nil)
-                        CGCompleteDisplayConfiguration(token, .forSession)
-                        completion()
-                    }
-                    break
+        var display = Display.main
+        let current = display.mode
+        let modes = display.modes
+        for mode in modes {
+            if mode != current {
+                display.mode = mode
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now().advanced(by: .seconds(10))) {
+                    display.mode = current
+                    completion()
                 }
+                break
             }
         }
         
     }
-
 }
 
